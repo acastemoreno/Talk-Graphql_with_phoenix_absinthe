d9e32693ec9a17516da46bab339a2aa1e36f932a
Albert Castellano Moreno <albert@able.co>
2020-09-28 12:57:33 -0500

    Initial commit

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'GraphQL with Phoenix and Absinthe', 
+     subtitle: 'Conceptos, Experiencia en Desarrollo', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/graphql-logo.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Albert Castellano Moreno', 
+     company: 'For Elixir Developers Mexico', 
+     gplus: '', 
+     twitter: '', 
+     www: '', 
+     github: 'http://github.com/acastemoreno', 
+   }]
+};
